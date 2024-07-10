ab3849cae40e4214ecaa2939829233ba27115f9d
Segrey <176_v_a_v@mail.ru>
2024-07-10 10:11:25 +0700

    Add files via upload

---
+++ M2. N6_Dop.py
@@ -0,0 +1,34 @@
+
+def poisk_kratnih(tchislo):
+    spisok_kratnih = []
+    for i in range(2, tchislo+1):
+        if tchislo % i == 0:
+            spisok_kratnih.append(i)
+            spisok_kratnih.append(tchislo//i)
+    spisok_kratnih = set(spisok_kratnih)
+    spisok_kratnih.discard(1)
+    spisok_kratnih.discard(2)
+    return sorted(spisok_kratnih)
+
+def poisk_summ(spisok_kratnih):
+    rezult = ''
+    for i in range(1, max(spisok_kratnih)):
+        for tchislo in spisok_kratnih:
+            if i <= tchislo//2:
+                if i != tchislo-i:
+                    rezult += str(i) + str(tchislo-i)
+    return rezult
+
+perem = int(input('Введите значение: '))
+print(poisk_kratnih(perem))
+print(poisk_summ(poisk_kratnih(perem)))
+input()
+
+
+
+
+
+
+
+
+
