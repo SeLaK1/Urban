3cc7948a30e86aaa27d2562b09d2c52d1a199d1d
Segrey <176_v_a_v@mail.ru>
2024-07-22 21:28:22 +0700

    M3. N3.py

--- M3. N3.py
+++ M3. N3.py
@@ -1,8 +1,15 @@
-def print_params(*param_1):
-    print(*param_1)
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
 
 value_list = [1, 'строка', True]
 value_dict = {'a': 1, 'b': 'строка', 'c': True}
 value_list_2 = [1, 2]
 
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+print()
+print_params(*value_list)
+print_params(**value_dict)
+print()
 print_params(*value_list_2, 2123)
