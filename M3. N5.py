d03ace19ba5a04ddba98892c3a6cf0594592fb58
Segrey <176_v_a_v@mail.ru>
2024-07-24 23:22:36 +0700

    M3. N5.py

--- M3. N5.py
+++ M3. N5.py
@@ -1,11 +1,12 @@
 
 def proizv_cifr(digit):
-    str_digit = str(digit)
-    first = int(str_digit[0])
-    if len(str_digit) == 1:
-        return digit
+    if len(str(digit)) == 1:
+        if digit == 0:
+            return 1
+        else:
+            return digit
     else:
-        return first * proizv_cifr(int(str_digit[1:]))
+        return int(str(digit)[0]) * proizv_cifr(int(str(digit)[1:]))
 
 digit = int(input('Введите значение: '))
 print(f'Результат числа {digit}: {proizv_cifr(digit)}')
