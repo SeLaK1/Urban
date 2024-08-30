e1cd332ccf15c18d5b13c0a386ea3efb0bda0666
Segrey <176_v_a_v@mail.ru>
2024-08-30 18:45:35 +0300

    M7. N2.py

--- M7. N2.py
+++ M7. N2.py
@@ -1,18 +1,14 @@
 
 def custom_write(file_name, strings):
     file = open(file_name, 'w', encoding='utf-8')
-    for elements in strings:
-        file.write(f'{elements}\n')
-    file.close()
-
-    file = open(file_name, 'r', encoding='utf-8')
     strings_positions = {}
-    count_lines, count_butes = 0, 0
-    for line in file:
+    count_lines = 0
+    for element in strings:
         count_lines += 1
-        print(file.tell())
-        strings_positions[(count_lines, count_butes)] = line[:-1]
+        strings_positions[count_lines, file.tell()] = element
+        file.write(f'{element}\n')
 
+    file.close()
     return strings_positions
 
 info = [
