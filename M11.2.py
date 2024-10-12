25619058a0271fd94c00c659b1e07896658f30c6
Segrey <176_v_a_v@mail.ru>
2024-10-12 22:30:44 +0300

    M11 N2.py

--- M11.2.py
+++ M11.2.py
@@ -1,9 +1,9 @@
+import inspect
 import sys
 from pprint import pprint
-from pip._vendor import requests
 def introspection_info(element):
     dict_ = {}
-    spisok_nyznogo = ['module', 'class', 'type', 'int', 'str', 'function', 'builtin_function_or_method',
+    spisok_nyznogo = ['module', 'class', 'type', 'int', 'str', 'real', 'function', 'builtin_function_or_method',
                       'method-wrapper', 'dict', 'list']
     for atrebut in dir(element):
         atrebut_info = getattr(element, atrebut)
@@ -11,7 +11,7 @@ def introspection_info(element):
         if stroka[1] in spisok_nyznogo:
             if stroka[1] == 'type' and (isinstance(element, int) or isinstance(element, str)):
                 dict_[type.__name__] = str(type(element)).split("'")[1]
-            elif stroka[1] == 'int' or stroka[1] == 'str' or stroka[1] == 'dict' or stroka[1] ==  'list':
+            elif stroka[1] == 'int' or stroka[1] == 'str' or stroka[1] == 'dict' or stroka[1] == 'list':
                 dict_.setdefault('attributes', [])
                 dict_['attributes'].append(atrebut)
             elif stroka[1] != 'slot' and stroka[0][1:] != 'built-in':
@@ -20,7 +20,7 @@ def introspection_info(element):
             else:
                 dict_.setdefault(stroka[1], [])
                 dict_[stroka[1]].append(atrebut)
+    dict_['modul'] = str(inspect.getmodule(introspection_info)).split("'")[1]
     return dict_
 
-
-pprint(introspection_info(9))
+pprint(introspection_info(9)
