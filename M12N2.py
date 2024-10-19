17c73a3d44f110d37f1d27d0f9d848bfad40465b
Segrey <176_v_a_v@mail.ru>
2024-10-19 22:03:32 +0300

    M12N2.py

--- M12N2.py
+++ M12N2.py
@@ -4,19 +4,20 @@ import unittest
 
 class TournmentTest(unittest.TestCase):
 
-    def setUp(self):
-        self.r1 = unitest2.Runner('Усэйн', 10)
-        self.r2 = unitest2.Runner('Андрей', 9)
-        self.r3 = unitest2.Runner('Ник', 4)
-
     @classmethod
     def setUpClass(self):
         self.all_results = {}
 
-    @classmethod
-    def tearDownClass(self):
+    def ttearDownClass(self):
+        rez = {}
         for key in self.all_results:
-            pprint(key, self.all_results[key])
+            rez[key] = self.all_results[key].name
+        print(rez)
+
+    def setUp(self):
+        self.r1 = unitest2.Runner('Усэйн', 10)
+        self.r2 = unitest2.Runner('Андрей', 9)
+        self.r3 = unitest2.Runner('Ник', 4)
 
     def TournamentTest(self, *partisipants):
         t1 = unitest2.Tournament(90, *partisipants)
@@ -30,10 +31,14 @@ class TournmentTest(unittest.TestCase):
                        self.all_results[key2] = perem
         return self.all_results
 
+
     def test_True(self):
-        self.assertEqual(self.TournamentTest(self.r1, self.r3)[2].name, 'Ник')
-        self.assertEqual(self.TournamentTest(self.r3, self.r2)[2].name, 'Ник')
-        self.assertEqual(self.TournamentTest(self.r1, self.r2, self.r3)[3].name, 'Ник')
+        self.assertEqual(self.TournamentTest(self.r1, self.r3)[self.all_results.__len__()].name, 'Ник')
+        self.ttearDownClass()
+        self.assertEqual(self.TournamentTest(self.r3, self.r2)[self.all_results.__len__()].name, 'Ник')
+        self.ttearDownClass()
+        self.assertEqual(self.TournamentTest(self.r1, self.r2, self.r3)[self.all_results.__len__()].name, 'Ник')
+        self.ttearDownClass()
 
 if __name__ == '__main__':
     unittest.main()
