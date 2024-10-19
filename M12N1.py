eb5b199ebfe6d36c82da0e53bf830e4214b919f0
Segrey <176_v_a_v@mail.ru>
2024-10-19 23:20:44 +0300

    M12N1.py

--- M12N1.py
+++ M12N1.py
@@ -1,19 +1,24 @@
-import unitest
+import unitest1
 import unittest
 
 class RunnerTest(unittest.TestCase):
-    runner1 = unitest.Runner('Sergio')
-    runner2 = unitest.Runner('Banana')
+    is_frozen = False
+    runner1 = unitest1.Runner('Sergio')
+    runner2 = unitest1.Runner('Banana')
+
+    @unittest.skipIf(is_frozen, 'Данный тест заморожен')
     def test_walk(self):
         for _ in range(10):
             self.runner1.walk()
         self.assertEqual(self.runner1.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Данный тест заморожен')
     def test_run(self):
         for _ in range(10):
             self.runner2.run()
         self.assertEqual(self.runner2.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Данный тест заморожен')
     def test_sravnenie(self):
         self.assertNotEqual(self.runner1.distance, self.runner2.distance)
 
