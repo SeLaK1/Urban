969742657aa28d604965366f9e80fd21676f5cef
Segrey <176_v_a_v@mail.ru>
2024-12-02 13:40:37 +0300

    M16_N3.py

--- M16_N3.py
+++ M16_N3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Form
 
 app = FastAPI()
 
@@ -13,18 +13,17 @@ async def get_all_user() -> dict:
 async def create_user(username: str, age: int) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f'Имя: {username}, возраст: {age}'
-    return f'User {username} is registred'
+    return f'User {current_index} is registred'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> str:
+async def update_user(user_id: int, username: str, age: int) -> str: # Form(..) - обязательная переменная, Form(None) - не обязательная переменная
     users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'User {username} is updated'
+    return f'User {user_id} is updated'
 
 @app.delete('/user/{user_id}')
 async def delete_message(user_id: int) -> str:
-    users.pop(user_id)
+    users.pop(str(user_id))
     return f'User with {user_id} was deleted'
-
 
 
 
