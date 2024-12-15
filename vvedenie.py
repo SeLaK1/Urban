1fa49abbc55483a420f20119c65b98ee9fb7b41c
Segrey <176_v_a_v@mail.ru>
2024-12-15 11:03:59 +0300

    Add files via upload

---
+++ vvedenie.py
@@ -0,0 +1,23 @@
+
+# 1st program
+print(9**0.5 *5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(a)
+print(b)
+print(a == b)
+
+# 4th program
+tchislo_1 = float('123.456')
+tchislo_2 = int(tchislo_1*10)
+print(tchislo_2 % 10)
+# stroka = '123.456'
+# for i in range(len(stroka)):
+#    if stroka[i-1] == '.':
+#        print(stroka[i])
+
